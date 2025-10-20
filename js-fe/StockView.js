d1b78bea5f06d726ee999788bc49085736df964d
d_wisarut <d.wisarut@hotmail.com>
2025-10-20 16:24:09 +0700

    feat: make new StockView for controlling elements in frontend

--- js-fe/StockView.js
+++ js-fe/StockView.js
@@ -1,3 +1,18 @@
 export default class StockView {
+    constructor(root, { onItemSelect, onItemAdd, onItemDelete }) {
+        this.root = root;
+        this.onItemSelect = onItemSelect;
+        this.onItemAdd = onItemAdd;
+        this.onItemDelete = onItemDelete;
 
+        this.tableBody = document.root.querySelector('table tbody');
+        this.addBtn = document.root.querySelector('.add-btn');
+        this.deleteBtn = document.root.querySelector('.delete-btn');
+        this.nameInput = document.root.querySelector('.namebar');
+
+        this.addBtn.addEventListener('click', () => {
+            this.onItemAdd;
+        })
+
+    }
 }
