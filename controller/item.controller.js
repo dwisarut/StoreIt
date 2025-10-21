983f620f82a8a1ed17fd9c04d2d45e8dd42d3ba3
d_wisarut <d.wisarut@hotmail.com>
2025-10-21 15:47:03 +0700

    fix: update variable name to work with frontend

--- controller/item.controller.js
+++ controller/item.controller.js
@@ -31,37 +31,47 @@ const createItem = async (req, res) => {
     const item = await Item.create(req.body);
     res.status(200).json(item);
   } catch (error) {
+    if (error.name === 'ValidationError' || error.code === 11000) {
+      return res.status(400).json({ message: error.message })
+    }
     res.status(500).json({ message: error.message });
   }
 };
 
 const updateItem = async (req, res) => {
   try {
-    const { id: partId } = req.params;
-    const updatedItem = await Item.findOneAndUpdate({ partId }, req.body, {
+    const { id } = req.params;
+    const updatedItem = await Item.findOneAndUpdate({ _id: id }, req.body, {
       new: true,
+      runValidators: true,
     });
 
     if (!updatedItem) {
       return res
         .status(404)
-        .json({ message: `Item with an ID ${partId} not found` });
+        .json({ message: `Item with an ID ${id} not found` });
     }
     res.status(200).json(updatedItem);
   } catch (error) {
+    console.error("Mongoose Error during item update:", error);
+
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ message: error.message });
+    }
+
     res.status(500).json({ message: error.message });
   }
 };
 
 const deleteItem = async (req, res) => {
   try {
-    const { id: partId } = req.params;
-    const deletedItem = await Item.findOneAndDelete({ partId });
+    const { id } = req.params;
+    const deletedItem = await Item.findOneAndDelete({ _id: id });
 
     if (!deletedItem) {
       return res
         .status(404)
-        .json({ message: `Item with an ID ${partId} not found` });
+        .json({ message: `Item with an ID ${id} not found` });
     }
     res.status(200).json({ message: "Item deleted succesfully" });
   } catch (error) {
